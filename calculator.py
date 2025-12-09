894dede8a7089ed75cfd056a5d8bed1c2d453b73
Md Adnan Khan <mdadnankhan783@gmail.com>
2025-12-09 11:48:27 +0530

    feat: add multiplication

--- calculator.py
+++ calculator.py
@@ -5,6 +5,9 @@ class Calculator:
     def subtract(self, x, y):
         return x - y
 
+    def multiply(self, x, y):
+        return x * y
+
 if __name__ == "__main__":
     calc = Calculator()
     print("Welcome to Calculator!")
